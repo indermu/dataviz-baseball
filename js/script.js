577772c19270e64893f449e123f8d02724ed1b03
H <tsaihu@Jour-TsaiHu.local>
2015-02-07 12:34:20 -0600

    change and add files

--- js/script.js
+++ js/script.js
@@ -1,6 +1,34 @@
+
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+    getData ();
 });
+
+function getData (){
+	$.getJSON ("js/Assignment 1 baseball player_Tsai.json", function(data) {
+		playerData= data;
+		drawChart();
+	});
+}
+
+function drawChart () {
+	 $(".player-name").html(playerData.name);
+
+	 $.each(playerData.stats, function (i, item) {
+
+	 	var width= item.HR * 10;
+
+	 {$.("chart").append (
+	 	 	"<div class='col-md-12 bar-container'>"+
+	 	 	"<div class='year'>"+item.year+"</div>"+
+	 	 	"<div class='bar' style='width: "+width+"px'></div>"+
+	 	 	"<div class='total'>"+item.HR+"</div>"+
+	 	 </div>"" 
+	 );
+
+	 }); 
+}
 
 
 
